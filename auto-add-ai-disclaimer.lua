b242c591aec8634754b2c27119c4cadc4557bce9
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-06-11 14:15:06 -0400

    Slight edits to disclaimer based on team review
    Co-authored-by: Kate Isaac <41767733+kweav@users.noreply.github.com>

--- auto-add-ai-disclaimer.lua
+++ auto-add-ai-disclaimer.lua
@@ -8,11 +8,12 @@ function Div(el)
 **AI chatbot use Disclaimer:**
 
 * Never provide sensitive data to a public AI chatbot.
-* Be specific in your prompts and ask the chatbot to help you understand code not simply just provide it.
+* Be specific in your prompts and ask the chatbot to help you understand code, instead of simply providing it.
 * Recognize that you may need to iteratively improve your prompt to reach the desired output.
-* Chatbots may produce inaccurate, out-of-date, or incomplete information. Always evaluate their responses critically, test generated code, and validate information using reliable sources.
+* The confidence of a chatbot does not indicate if its responses are correct. They may also agree with you when you are wrong.
+* Chatbots may produce inaccurate, out-of-date, incomplete, or unnecessarily complicated information. Always evaluate their responses critically, test and simplify generated code, and validate information using reliable sources.
 * Asking a chatbot the same question more than once (even with the same prompt!) can result in different answers, so be sure to document what you can for the sake of reproducibility.
- * Keep track of the models you use (including versions) and be transparent about it your AI use in your work.
+* Keep track of the models you use (including versions) and be transparent about AI use in your work.
      ]]
 
     -- Parse Markdown into Pandoc AST blocks
