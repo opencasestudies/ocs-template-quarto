5122d616b9d04712d3bfe5e5dd917b2bd0098b83
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-07-23 12:27:46 -0400

    Apply suggestions from code review to sync style
    Co-authored-by: Carrie Wright <23014755+carriewright11@users.noreply.github.com>

--- auto-add-ai-disclaimer.lua
+++ auto-add-ai-disclaimer.lua
@@ -7,21 +7,21 @@ function Div(el)
     local md = [[
 **AI Use Disclaimer:**
 
-* Follow any relevant required guidelines/policies for AI use
+* Follow any relevant required guidelines/policies for AI use.
 <details> <summary>Examples</summary> Your team or lab, institution, funding organization, or publisher. These requirements should take precedence over our suggestions. </details>
 
-* Check AI responses critically, as they may be inaccurate, out-of-date, incomplete, or unnecessarily complicated 
+* Check AI responses critically, as they may be inaccurate, out-of-date, incomplete, or unnecessarily complicated.
     <details><summary>Examples </summary> 
  
-    * Always validate the existence and relevance of suggested citations or packages
-    * Review, test, possibly simplify, and work to understand generated code or functions
-    * Validate factual information using reliable sources
+    * Always validate the existence and relevance of suggested citations or packages.
+    * Review, test, possibly simplify, and work to understand generated code or functions.
+    * Validate factual information using reliable sources.
  
     </details> 
-* Exercise extra caution with code in certain instances: code that installs packages, accesses or modifies files or data, uses credentials, or sends information over the internet
-* [Disclose](https://aiattribution.github.io/) your AI use (including model and versions)
+* Exercise extra caution with code in certain instances: code that installs packages, accesses or modifies files or data, uses credentials, or sends information over the internet.
+* [Disclose](https://aiattribution.github.io/) your AI use (including model and versions).
 * Never provide sensitive data to a public AI tool. <details><summary>Examples</summary> Patient or student data, passwords, credentials, API keys, unpublished manuscripts or grants, and other proprietary code or internal documents.</details> 
-* Be specific in your prompts and ask the chatbot and to help you understand the code
+* Be specific in your prompts and ask the AI tool to help you understand the code.
 * The confidence of an AI tool does not indicate if its responses are correct. They may also agree with you when you are wrong.
 * AI may respond differently to the same repeated prompt. Be sure to document what you can for the sake of reproducibility.
 * Recognize that you may need to iteratively improve your prompt to reach the desired output.
